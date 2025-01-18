d332e8fc052ea9727fb77614c5f9b4b4acba48cf
Anass Akera <50927517+anassakera@users.noreply.github.com>
2025-01-18 13:22:54 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'constant/app_theme.dart';
 import 'constant/constants.dart';
-import 'routes/app_routes.dart';
+import 'screens/dashboard/dashboard_screen.dart';
 // import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -33,9 +33,10 @@ class MyApp extends StatelessWidget {
       supportedLocales: Constants.supportedLanguages.map(
         (lang) => Locale(lang),
       ),
-      initialRoute: AppRoutes.splashScreen,
-      routes: AppRoutes.getRoutes(),
-      onUnknownRoute: AppRoutes.onGenerateRoute,
+      home: const DashboardScreen(),
+      // initialRoute: AppRoutes.splashScreen,
+      // routes: AppRoutes.getRoutes(),
+      // onUnknownRoute: AppRoutes.onGenerateRoute,
     );
   }
 }
